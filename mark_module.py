e4205828ba1000b79f350b2d711795eab0356aeb
Mark Blackwelder <mark.jon.blackwelder.ii@gmail.com>
2024-06-22 10:02:12 -0400

    Projectile code

--- mark_module.py
+++ mark_module.py
@@ -3,9 +3,23 @@ import sys
 import random
 import time
 
-class Projectile:
-    def __init__(self,):
-
+# class Projectile:
+#     #insert a ", x, y" in instances
+#     def __init__(self, screen):
+#     self.screen = screen
+#     # self.x = Hero.x
+#     # self.y = Hero.y
+#
+#     def draw(self):
+#         pygame.draw.circle(screen, )
+#
+#     def move(self):
+#
+#     def off_screen(self):
+#
+#
+#     def hits(self):
+#         projecticle
 def main():
     # turn on pygame
     pygame.init()
