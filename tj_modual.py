292718e3d59a258830acf92c5540257a31e24469
Mark Blackwelder <mark.jon.blackwelder.ii@gmail.com>
2024-06-25 14:00:04 -0400

    Hero and projectile integrated

--- tj_modual.py
+++ tj_modual.py
@@ -4,6 +4,7 @@ import random
 import time
 import Hero_Module
 import mark_module
+import math
 
 
 # need counsalur imagise (mabye as caricter salection) tj on free time
@@ -14,97 +15,130 @@ import mark_module
 
 
 class Cammperhealthy:
-    def __init__(self, screen, x, y,):
+    def __init__(self, screen, x, y):
         self.screen = screen
         self.x = x
         self.y = y
-        self.speedx = 3((Hero_Module.Hero.x - self.x)/(abs(Hero_Module.Hero.x- self.x)))
-        self.speedy = 3((Hero_Module.Hero.self.y - self.y)/(abs(Hero_Module.Hero.self.y- self.y)))
         self.imageunharmed = pygame.image.load("sprites/download-Photoroom.png")
         self.imageunharmedright = pygame.image.load("sprites/download-Photoroom.png")
-        self.imageunharmedleft =pygame.image.load("sprites/download-Photoroom.png")
+        self.imageunharmedleft = pygame.image.load("sprites/download-Photoroom.png")
         self.imageunharmedup =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageunharmeddown =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageunharmedleftdown =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageunharmedleftup =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageunharmedrightup =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageunharmedrightdown =pygame.image.load("sprites/download-Photoroom.png")
-        self.projectilehits =0
+        self.imageunharmeddown = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageunharmedleftdown = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageunharmedleftup = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageunharmedrightup = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageunharmedrightdown = pygame.image.load("sprites/download-Photoroom.png")
+        self.hit_box_unharmed_left = pygame.draw.rect()
 
     def move(self):
+        self.speedx = 3 * ((Hero_Module.Hero.x - self.x) / (abs(Hero_Module.Hero.x - self.x)))
+        self.speedy = 3 * ((Hero_Module.Hero.y - self.y) / (abs(Hero_Module.Hero.y - self.y)))
         self.x = self.x + self.speedx
         self.x = self.y + self.speedy
 
 
     def draw(self):
-        if self.projectilehits == 0 and self.speedx == 0 and self.speedy == 0:
+        self.speedx = 3 * ((Hero_Module.Hero.x - self.x) / (abs(Hero_Module.Hero.x - self.x)))
+        self.speedy = 3 * ((Hero_Module.Hero.y - self.y) / (abs(Hero_Module.Hero.y - self.y)))
+        if self.speedx == 0 and self.speedy == 0:
             self.screen.blit(self.imageunharmed, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx >= 0 and self.speedy == 0:
+        elif self.speedx >= 0 and self.speedy == 0:
             self.screen.blit(self.imageunharmedright, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx <= 0 and self.speedy == 0:
+        elif self.speedx <= 0 and self.speedy == 0:
             self.screen.blit(self.imageunharmedleft, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx == 0 and self.speedy >= 0:
+        elif self.speedx == 0 and self.speedy >= 0:
             self.screen.blit(self.imageunharmeddown, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx == 0 and self.speedy <= 0:
+        elif self.speedx == 0 and self.speedy <= 0:
             self.screen.blit(self.imageunharmedup, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx >= 0 and self.speedy >= 0:
+        elif self.speedx >= 0 and self.speedy >= 0:
             self.screen.blit(self.imageunharmedleftdown, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx <= 0 and self.speedy >= 0:
+        elif self.speedx <= 0 and self.speedy >= 0:
             self.screen.blit(self.imageunharmedrightdown, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx >= 0 and self.speedy <= 0:
+        elif self.speedx >= 0 and self.speedy <= 0:
             self.screen.blit(self.imageunharmedleftup, (self.x, self.y))
-        elif self.projectilehits == 0 and self.speedx <= 0 and self.speedy <= 0:
+        else:
             self.screen.blit(self.imageunharmedrightup, (self.x, self.y))
+
+        def hitby():
+            self.speedx = 3 * ((Hero_Module.Hero.x - self.x) / (abs(Hero_Module.Hero.x - self.x)))
+            self.speedy = 3 * ((Hero_Module.Hero.y - self.y) / (abs(Hero_Module.Hero.y - self.y)))
+            if self.speedx == 0 and self.speedy == 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmed.get_width(),
+                                           self.imageunharmed.get_height())
+            elif self.speedx >= 0 and self.speedy == 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmedright.get_width(),
+                                           self.imageunharmedright.get_height())
+            elif self.speedx <= 0 and self.speedy == 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmedleft.get_width(),
+                                           self.imageunharmedleft.get_height())
+            elif self.speedx == 0 and self.speedy >= 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmeddown.get_width(),
+                                self.imageunharmeddown.get_height())
+            elif self.speedx == 0 and self.speedy <= 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmedup.get_width(),
+                                self.imageunharmedup.get_height())
+            elif self.speedx >= 0 and self.speedy >= 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmedleftdown.get_width(),
+                                           self.imageunharmedleftdown.get_height())
+            elif self.speedx <= 0 and self.speedy >= 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmedrightdown.get_width(),
+                                 self.imageunharmedrightdown.get_height())
+            elif self.speedx >= 0 and self.speedy <= 0:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmedleftup.get_width(),
+                                           self.imageunharmedleftup.get_height())
+            else:
+                self.hit_box = pygame.Rect(self.x, self.y, self.imageunharmedrightup.get_width(),
+                                           self.imageunharmedrightup.get_width())
+
+
 class Cammperhurt:
-    def __init__(self, screen, x, y, ):
+    def __init__(self, screen, x, y):
         self.screen = screen
         self.x = x
         self.y = y
-        self.speedx = 1.5((Hero_Module.Hero.self.x - self.x)/(abs(Hero_Module.Hero.self.x- self.x)))
-        self.speedy = 1.5((Hero_Module.Hero.self.y - self.y)/(abs(Hero_Module.Hero.self.y- self.y)))
-        self.imageharmed =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmedright =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmedleft =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmedup =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmeddown =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmedleftdown =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmedleftup =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmedrightup =pygame.image.load("sprites/download-Photoroom.png")
-        self.imageharmedrightdown =pygame.image.load("sprites/download-Photoroom.png")
-        self.projectilehits =1
+        self.imageharmed = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmedright = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmedleft = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmedup = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmeddown = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmedleftdown = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmedleftup = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmedrightup = pygame.image.load("sprites/download-Photoroom.png")
+        self.imageharmedrightdown = pygame.image.load("sprites/download-Photoroom.png")
 
 
     def move(self):
-        self.x = self.x + self.speedx
-        self.y = self.y + self.speedy
+        self.x = self.x + 1.5 * ((Hero_Module.Hero.x - self.x)/(abs(Hero_Module.Hero.x - self.x)))
+        self.y = self.y + 1.5 * ((Hero_Module.Hero.y - self.y)/(abs(Hero_Module.Hero.y - self.y)))
 
     def draw(self):
-        if self.projectilehits == 1 and self.speedx == 0 and self.speedy == 0:
+        self.speedx = 1.5 * ((Hero_Module.Hero.x - self.x)/(abs(Hero_Module.Hero.x - self.x)))
+        self.speedy = 1.5 * ((Hero_Module.Hero.y - self.y)/(abs(Hero_Module.Hero.y - self.y)))
+        if self.speedx == 0 and self.speedy == 0:
             self.screen.blit(self.imageharmed, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx >= 0 and self.speedy == 0:
+        elif self.speedx >= 0 and self.speedy == 0:
             self.screen.blit(self.imageharmedright, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx <= 0 and self.speedy == 0:
+        elif self.speedx <= 0 and self.speedy == 0:
             self.screen.blit(self.imageharmedleft, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx == 0 and self.speedy >= 0:
+        elif self.speedx == 0 and self.speedy >= 0:
             self.screen.blit(self.imageharmeddown, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx == 0 and self.speedy <= 0:
+        elif self.speedx == 0 and self.speedy <= 0:
             self.screen.blit(self.imageharmedup, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx >= 0 and self.speedy >= 0:
+        elif self.speedx >= 0 and self.speedy >= 0:
             self.screen.blit(self.imageharmedleftdown, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx <= 0 and self.speedy >= 0:
+        elif self.speedx <= 0 and self.speedy >= 0:
             self.screen.blit(self.imageharmedrightdown, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx >= 0 and self.speedy <= 0:
+        elif self.speedx >= 0 and self.speedy <= 0:
             self.screen.blit(self.imageharmedleftup, (self.x, self.y))
-        elif self.projectilehits == 1 and self.speedx <= 0 and self.speedy <= 0:
+        elif self.speedx <= 0 and self.speedy <= 0:
             self.screen.blit(self.imageharmedrightup, (self.x, self.y))
+
 class Cammperdead:
     def __init__(self, screen, x, y):
         self.screen = screen
         self.x = x
         self.y = y
-        self.imagedead =pygame.image.load("sprites/download-Photoroom.png")
-
-        self.projectilehits =2
+        self.imagedead = pygame.image.load("sprites/download-Photoroom.png")
 
 
 
@@ -127,64 +161,90 @@ def main():
     # TODO: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((640, 480))
     width = screen.get_width()
-    hight = screen.get_height()
+    height = screen.get_height()
     # let's set the framerate
     clock = pygame.time.Clock()
-    clock2 = clock
+    clock2 = 0
     hurt = []
     healthy = []
     dead = []
-    round = clock2/10
-    spawns = round*5
-    if clock%10:
-        spawnernumber = random.randint(0, "spawns")
-        waveside = random.randint(1,4)
-        if waveside == 1:
-            spawnsidex = random.randint(1, "width")
-            spawnsidey = 1
-        if waveside == 2:
-            spawnsidex = random.randint(1, "width")
-            spawnsidey = hight
-        if waveside == 3:
-            spawnsidex = 0
-            spawnsidey = random.randint(1, "hight")
-        if waveside == 4:
-            spawnsidex = width
-            spawnsidey = random.randint(1, "hight")
-        for i in range(1, spawnernumber):
-            camperhurt = Cammperhurt(screen, (Cammperhealthy.self.x, Cammperhealthy.self.y), )
-            hurt.append(camperhurt)
-        for i in range(1, (spawns-spawnernumber)/2):
-            camperhealthy = Cammperhealthy(screen, (spawnsidex, spawnsidey))
-            healthy.append(camperhealthy)
-    if Cammperhealthy.hitby(mark_module.Projectile):
-        for i in range(1,1):
-            hurtcamper = Cammperhurt(screen, (Cammperhealthy.self.x, Cammperhealthy.self.y), )
-            hurt.append(hurtcamper)
-            healthy.remove()
-    if Cammperhurt.hitby(mark_module.Projectile):
-        for i in range(1, len(dead)):
-            camperdead = Cammperdead(screen, (Cammperhurt.self.x, Cammperhurt.self.y) )
-            dead.append(camperdead)
-            hurt.remove()
+
+
+    # for
+    #     if Cammperhealthy.hitby(mark_module.Projectile):
+    #     hurtcamper = Cammperhurt(screen, (Cammperhealthy.self.x, Cammperhealthy.self.y), )
+    #     hurt.append(hurtcamper)
+    #     healthy.remove()
+    # if Cammperhurt.hitby(mark_module.Projectile):
+    #     for i in range(1, len(dead)):
+    #         camperdead = Cammperdead(screen, (Cammperhurt.self.x, Cammperhurt.self.y) )
+    #         dead.append(camperdead)
+    #         hurt.remove()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         clock.tick(60)
 
+        if not (time.time() - clock2) % 10:
+            spawns = math.floor((time.time() - clock2) / 2)
+            spawnernumber = random.randint(0, spawns)
+            waveside = random.randint(1, 4)
+            if waveside == 1:
+                for i in range(0, spawnernumber):
+                    camperhurt = Cammperhurt(screen, random.randint(1, width), 1)
+                    hurt.append(camperhurt)
+                    spawnernumber -= 1
+                for i in range(0, math.floor(spawnernumber / 2)):
+                    camperhealthy = Cammperhealthy(screen, random.randint(0, width), 0)
+                    healthy.append(camperhealthy)
+            elif waveside == 2:
+                for i in range(0, spawnernumber):
+                    camperhurt = Cammperhurt(screen, random.randint(1, width), height)
+                    hurt.append(camperhurt)
+                    spawnernumber -= 1
+                for i in range(0, math.floor(spawnernumber / 2)):
+                    camperhealthy = Cammperhealthy(screen, random.randint(1, width), height)
+                    healthy.append(camperhealthy)
+            elif waveside == 3:
+                for i in range(0, spawnernumber):
+                    camperhurt = Cammperhurt(screen, 0, random.randint(0, height))
+                    hurt.append(camperhurt)
+                    spawnernumber -= 1
+                for i in range(0, math.floor(spawnernumber / 2)):
+                    camperhealthy = Cammperhealthy(screen, 0, random.randint(0, height))
+                    healthy.append(camperhealthy)
+            else:
+                for i in range(0, spawnernumber):
+                    camperhurt = Cammperhurt(screen, width, random.randint(0, height))
+                    hurt.append(camperhurt)
+                    spawnernumber -= 1
+                for i in range(0, math.floor(spawnernumber / 2)):
+                    camperhealthy = Cammperhealthy(screen, width, random.randint(0, height))
+                    healthy.append(camperhealthy)
             # TODO: Add you events code
         for camperhurt in hurt:
-            camperhurt.move()
-            camperhurt.draw()
+            for bullet in mark_module.projectiles:
+                if camperhurt.hitby()
+                    cammperdead = Cammperdead(screen, (camperhurt.x, camperhurt.y))
+                    dead.append(cammperdead)
+                    camperhurt.remove()
+                    bullet.remove()
+                else:
+                    camperhurt.move()
+                    camperhurt.draw()
         for camperhealthy in healthy:
-            camperhealthy.move()
-            camperhealthy.draw()
+            for bullet in mark_module.projectiles:
+                if camperhealthy.hitby(bullet):
+                    camperhurt = Cammperhurt(screen, (camperhealthy.x, camperhealthy.y))
+                    hurt.append(camperhurt)
+                    camperhealthy.remove()
+                    bullet.remove()
+                else:
+                    camperhealthy.move()
+                    camperhealthy.draw()
         for cammperdead in dead:
             cammperdead.draw()
-        for hurtcamper in hurt:
-            hurtcamper.move()
-            hurtcamper.draw()
         # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
 
