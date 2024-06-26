bb2f1df4d6f12fe08201c294f947d92378a2352f
collin <collinbrfaley@gmail@com>
2024-06-26 13:39:04 -0400

    In Class follow along

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ import math
 import Hero_Module
 import mark_module
 import opening_screen
-
+import tj_modual
 
 # need enemy
 
@@ -33,6 +33,31 @@ def main():
     bar = pygame.image.load("sprites/health_bar.png")
     pfp = opening_screen.Menu(screen)
     char_pfp = 0
+    width = screen.get_width()
+    height = screen.get_height()
+    clock2 = time.time()
+    clock3 = 0
+    hurt = []
+    healthy = []
+
+    harmedright = pygame.image.load("sprites/camper unhealthy right.png")
+    harmedleft = pygame.image.load("sprites/camper unhealthy left.png")
+    harmedup = pygame.image.load("sprites/camper unhealthy up.png")
+    harmeddown = pygame.image.load("sprites/camper unhealthy down.png")
+    harmedleftdown = pygame.image.load("sprites/camper unhealthy left down.png")
+    harmedleftup = pygame.image.load("sprites/camper unhealthy left up.png")
+    harmedrightup = pygame.image.load("sprites/camper unhealthy right up.png")
+    harmedrightdown = pygame.image.load("sprites/camper unhealthy right down.png")
+
+    unharmedright = pygame.image.load("sprites/camper healthy right.png")
+    unharmedleft = pygame.image.load("sprites/camper healthy left.png")
+    unharmedup = pygame.image.load("sprites/camper healthy up.png")
+    unharmeddown = pygame.image.load("sprites/camper healthy down.png")
+    unharmedleftdown = pygame.image.load("sprites/camper healthy left down.png")
+    unharmedleftup = pygame.image.load("sprites/camper healthy left up.png")
+    unharmedrightup = pygame.image.load("sprites/camper healthy right up.png")
+    unharmedrightdown = pygame.image.load("sprites/camper healthy right down.png")
+
     while True:
 
         while menu_state:
@@ -97,6 +122,79 @@ def main():
             the_hero.move(-5, 0)
         if pressed_keys[pygame.K_d]:
             the_hero.move(5, 0)
+
+
+        if time.time() - clock3 > 3:
+            if not math.floor(time.time() - clock2) % 5:
+                spawns = math.floor(time.time() - clock2) / 5
+                spawnernumber = int(spawns)
+                waveside = random.randint(1, 4)
+                if waveside == 1:
+                    for i in range(0, math.floor(spawnernumber / 2)):
+                        camperhealthy = tj_modual.Cammperhealthy(screen, random.randint(0, width), 0, unharmedright, unharmedleft, unharmedup, unharmeddown, unharmedleftdown, unharmedleftup, unharmedrightup, unharmedrightdown)
+                        healthy.append(camperhealthy)
+                        spawnernumber -= 2
+                    for i in range(0, spawnernumber):
+                        camperhurt = tj_modual.Cammperhurt(screen, random.randint(0, width), 0, harmedright, harmedleft,
+                                                           harmedup, harmeddown, harmedleftdown, harmedleftup,
+                                                           harmedrightup, harmedrightdown)
+                        hurt.append(camperhurt)
+                elif waveside == 2:
+                    for i in range(0, math.floor(spawnernumber / 2)):
+                        camperhealthy = tj_modual.Cammperhealthy(screen, random.randint(0, width), height, unharmedright, unharmedleft, unharmedup, unharmeddown, unharmedleftdown, unharmedleftup, unharmedrightup, unharmedrightdown)
+                        healthy.append(camperhealthy)
+                        spawnernumber -= 2
+                    for i in range(0, spawnernumber):
+                        camperhurt = tj_modual.Cammperhurt(screen, random.randint(0, width), height, harmedright, harmedleft, harmedup, harmeddown, harmedleftdown, harmedleftup, harmedrightup, harmedrightdown)
+                        hurt.append(camperhurt)
+                elif waveside == 3:
+                    for i in range(0, math.floor(spawnernumber / 2)):
+                        camperhealthy = tj_modual.Cammperhealthy(screen, 0, random.randint(0, height), unharmedright, unharmedleft, unharmedup, unharmeddown, unharmedleftdown, unharmedleftup, unharmedrightup, unharmedrightdown)
+                        healthy.append(camperhealthy)
+                        spawnernumber -= 2
+                    for i in range(0, spawnernumber):
+                        camperhurt = tj_modual.Cammperhurt(screen, 0, random.randint(0, height), harmedright, harmedleft, harmedup, harmeddown, harmedleftdown, harmedleftup, harmedrightup, harmedrightdown)
+                        hurt.append(camperhurt)
+                else:
+                    for i in range(0, math.floor(spawnernumber / 2)):
+                        camperhealthy = tj_modual.Cammperhealthy(screen, width, random.randint(0, height), unharmedright, unharmedleft, unharmedup, unharmeddown, unharmedleftdown, unharmedleftup, unharmedrightup, unharmedrightdown)
+                        healthy.append(camperhealthy)
+                        spawnernumber -= 2
+                    for i in range(0, spawnernumber):
+                        camperhurt = tj_modual.Cammperhurt(screen, width, random.randint(0, height), harmedright, harmedleft, harmedup, harmeddown, harmedleftdown, harmedleftup, harmedrightup, harmedrightdown)
+                        hurt.append(camperhurt)
+                clock3 = time.time()
+        camperstoremovehurt = []
+        for Camperhurt in hurt:
+            for bullet in the_hero.bullets:
+                if Camperhurt.hitby(bullet, the_hero.x, the_hero.y):
+                    if Camperhurt not in camperstoremovehurt:
+                        camperstoremovehurt.append(Camperhurt)
+                        the_hero.bullets.remove(bullet)
+            Camperhurt.move(the_hero.x, the_hero.y)
+            Camperhurt.draw(the_hero.x, the_hero.y)
+        for Camperhurt in camperstoremovehurt:
+            hurt.remove(Camperhurt)
+        camperstoremovehealthy = []
+        for camperhealthy in healthy:
+            for bullet in the_hero.bullets:
+                if camperhealthy.hitby(bullet, the_hero.x,the_hero.y):
+                    if camperhealthy not in camperstoremovehealthy:
+                        camperstoremovehealthy.append(camperhealthy)
+                        camperhurt = tj_modual.Cammperhurt(screen, camperhealthy.x, camperhealthy.y, harmedright,
+                                                           harmedleft, harmedup, harmeddown, harmedleftdown,
+                                                           harmedleftup, harmedrightup, harmedrightdown)
+                        hurt.append(camperhurt)
+                        the_hero.bullets.remove(bullet)
+            camperhealthy.move(the_hero.x, the_hero.y)
+            camperhealthy.draw(the_hero.x, the_hero.y)
+        for camperhealthy in camperstoremovehealthy:
+            healthy.remove(camperhealthy)
+
+
+
+
+
 
         #if event.type == pygame.MOUSEBUTTONDOWN:
         #    the_hero.shoot()
